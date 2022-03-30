8612bb1b8b6ed72c1c951fc299aa9c83662690c0
SOLER Lilian <soler.lilian64@gmail.com>
2022-03-31 00:23:07 +0200

    TP2  Angular - The to do list

--- 2-Pittion/Exercices/TP2/todo-list/src/app/app.component.ts
+++ 2-Pittion/Exercices/TP2/todo-list/src/app/app.component.ts
@@ -5,9 +5,23 @@ import { Component } from '@angular/core';
     <h1>Welcome to {{ title }}!</h1>
 
     <app-input-button-unit></app-input-button-unit>
+
+    <ul>
+      <li *ngFor="let todoItem of todoList">
+        {{ todoItem.title }}
+      </li>
+    </ul>
   `,
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'todo-list';
+  todoList = [
+    { title: 'install NodeJS' },
+    { title: 'install Angular CLI' },
+    { title: 'create new app' },
+    { title: 'serve app' },
+    { title: 'develop app' },
+    { title: 'deploy app' },
+  ];
 }
