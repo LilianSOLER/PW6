bfb8468331bce011749529645fedd7ba8e479fc2
SOLER Lilian <soler.lilian64@gmail.com>
2022-03-31 00:13:35 +0200

    TP2  Angular - Proprety binding

--- 2-Pittion/Exercices/TP2/todo-list/src/app/input-button-unit/input-button-unit.component.ts
+++ 2-Pittion/Exercices/TP2/todo-list/src/app/input-button-unit/input-button-unit.component.ts
@@ -2,22 +2,13 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-input-button-unit',
-  template: ` <p>input-button-unit works! The title is: {{ title }}</p> `,
+  template: ` <input [value]="title" /> `,
   styleUrls: ['./input-button-unit.component.scss'],
 })
 export class InputButtonUnitComponent implements OnInit {
-  title: string = 'Hello World';
+  title = 'Hello World';
 
-  constructor() {
-    console.log('in constructor');
-    this.changeTitle('My First Angular App');
-    console.log(this.title);
-  }
-
-  changeTitle(newTitle: string) {
-    console.log(newTitle);
-    this.title = newTitle;
-  }
+  constructor() {}
 
   ngOnInit(): void {}
 }
