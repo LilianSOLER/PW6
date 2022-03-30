ce5422a48b41d61b3b7b57e8d059e3a0c757cdd9
SOLER Lilian <soler.lilian64@gmail.com>
2022-03-31 00:20:16 +0200

    TP2  Angular - Element #

--- 2-Pittion/Exercices/TP2/todo-list/src/app/input-button-unit/input-button-unit.component.ts
+++ 2-Pittion/Exercices/TP2/todo-list/src/app/input-button-unit/input-button-unit.component.ts
@@ -3,10 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-input-button-unit',
   template: `
-    <p>input-button-unit works! The title is: {{ title }}</p>
+    <input
+      #inputElementRef
+      [value]="title"
+      (keyup.enter)="changeTitle($event.target.value)"
+    />
 
-    <input [value]="title" (keyup.enter)="changeTitle($event.target.value)" />
-    <button (click)="changeTitle('Button Clicked!')">Save</button>
+    <button (click)="changeTitle(inputElementRef.value)">Save</button>
   `,
   styleUrls: ['./input-button-unit.component.scss'],
 })
@@ -15,9 +18,9 @@ export class InputButtonUnitComponent implements OnInit {
 
   constructor() {}
 
+  ngOnInit(): void {}
+
   changeTitle(newTitle: string): void {
     this.title = newTitle;
   }
-
-  ngOnInit(): void {}
 }
