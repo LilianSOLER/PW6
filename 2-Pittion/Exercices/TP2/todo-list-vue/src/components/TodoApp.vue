fbed2522fb1ea0eece159c175d80178e33ce8b89
SOLER Lilian <soler.lilian64@gmail.com>
2022-04-10 09:12:48 +0200

    TP2 - Changement nom todo-list angular - Test todo-list avec vue ( création du component avec titre en param

---
+++ 2-Pittion/Exercices/TP2/todo-list-vue/src/components/TodoApp.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <h2>{{ title }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  title: '',
+  props: {
+    title: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.container {
+  margin: 0 auto;
+  max-width: 600px;
+}
+
+.container h2 {
+  text-align: center;
+}
+</style>
